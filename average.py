26203bbd4b24c0064a205e3cf7a3ff72f5d3702f
krishna9258 <krishna.gla1_bca24@gla.ac.in>
2025-08-20 13:07:25 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,8 @@
+a = int(input("enter number a: "))
+b = int(input("enter number b: "))
+c = int(input("enter number c: "))
+d = int(input("enter number d: "))
+e = int(input("enter number e: "))
+sum = a+b+c+d
+avg = sum/5
+print(avg)
